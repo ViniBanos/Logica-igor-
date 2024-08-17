8a259c9d791d0129ac6bbd38262e1cad0998e21e
Vinicius <129288327+canudooo@users.noreply.github.com>
2024-08-17 00:30:22 -0300

    Update Atividade Json.py

--- Atividade Json.py
+++ Atividade Json.py
@@ -1,37 +1,43 @@
-sum_das_notas = sum(notas.values())
-n_de_disciplinas = len(notas)
-media = sum_das_notas / n_de_disciplinas
-
-pessoas = [ 
+pessoas = [
     {
-        "nome" : "Igor",
+        "nome": "Igor",
         "idade": "20 anos",
         "id": "SP12345",
-        "notas":{
+        "notas": {
             "portugues": 6,
             "matematica": 9,
             "ciencias": 2,
-            "media": 10
         }
     },
-
     {
-        "nome" : "Ana",
+        "nome": "Ana",
         "idade": "19 anos",
         "id": "SP54321",
         "notas": {
             "portugues": 3,
             "matematica": 3,
             "ciencias": 10,
-            "media": 10
         }
     }
 ]
 
+def calc_media(pessoa):
+    notas = pessoa["notas"]
+    sum_das_notas = sum(notas.values())
+    n_de_disciplinas = len(notas)
+    media = sum_das_notas / n_de_disciplinas
+    return media
+
 def get_pessoas():
-    return pessoas
+    print(pessoas)
 
-print (pessoas())
+def add_pessoa():
+    pessoa = {}
+    for i in pessoas[0].keys():
+        print('Digite o Campo ', i)
+        valor = input()
+        pessoa[i] = valor
+    pessoas.append(pessoa)
 
-def add_pessoas():
-    return 
+add_pessoa()
+get_pessoas()
