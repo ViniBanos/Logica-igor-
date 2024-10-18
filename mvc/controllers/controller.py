435c661e7e8081b8ae9ce615fb65620e1921b913
Vinicius <129288327+ViniBanos@users.noreply.github.com>
2024-10-17 22:24:59 -0300

    Update controller.py

--- mvc/controllers/controller.py
+++ mvc/controllers/controller.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from models.model import skyline
+from flask import Blueprint, render_template, request
+from models.model import Usuario, lista
 
 pagina_simples = Blueprint('simple_page', __name__)
 
@@ -7,6 +7,12 @@ pagina_simples = Blueprint('simple_page', __name__)
 def mostrar():
     return render_template('index.html')
 
-@pagina_simples.route('/carro')
-def mostrarCarro():
-    return render_template('index.html', carro = skyline)
+@pagina_simples.route('/login', methods = ['POST'])
+def pegar():
+    name = request.form["name"]
+    senha = request.form["senha"]
+    for pessoa in lista:
+        if name == pessoa.nome:
+            if senha == pessoa.senha:
+                return 'Logado Games'
+    return 'Não logado Games'
