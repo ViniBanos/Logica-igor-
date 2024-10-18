4fa7ae2901724287bd1e1abc400c627e6ac46453
Vinicius <129288327+ViniBanos@users.noreply.github.com>
2024-10-17 22:24:41 -0300

    Update app.py

--- mvc/app.py
+++ mvc/app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, session, request, redirect, url_for
 from controllers.controller import pagina_simples
 
 app = Flask(__name__)
+app.secret_key = 'chave_secreta'
 app.register_blueprint(pagina_simples)
 
 @app.route("/")
 def hello():
-    return "Hello Word!"
+    return render_template('index.html')
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login ():
+    if request.method == 'POST':
+        session ['name'] = request.form['name']
+        return redirect(url_for ('hello'))
+    return  
 
 if __name__ == '__main__':
     app.run(debug=True)
