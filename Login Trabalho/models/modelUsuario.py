71cb2a759b4c23bd63df8fca4356f0a52d8744f0
Vinicius <129288327+ViniBanos@users.noreply.github.com>
2024-11-01 23:22:50 -0300

    Add files via upload
    medo

---
+++ Login Trabalho/models/modelUsuario.py
@@ -0,0 +1,11 @@
+class Usuario:
+    def __init__(self, nome, senha, id):
+        self.nome = nome
+        self.senha = senha
+        self.id = id 
+        
+usuarios = []
+pedro = Usuario("Pedro", "19", 1)
+usuarios.append(pedro)
+julia = Usuario('Julia', '10', 0)
+usuarios.append(julia)
